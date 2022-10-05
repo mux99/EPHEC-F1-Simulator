ad91718ec72afe5b857548a726b542b0413d3861
maxime <mux99@live.be>
2022-10-05 11:49:18 +0200

    worked a bit

--- projet.c
+++ projet.c
@@ -6,7 +6,7 @@ int car_nums[] = {44, 63, 1, 11, 55, 16, 4, 3, 14, 31, 10, 22, 5, 18, 6, 25, 77,
 
 char* locations[] = {"Bharain", "Saudi Arabian", "Australian", "Emilia Romagna", "Miami", "Spanish", "Monaco", "Azerbaijan", "Canadian", "British", "Austrian", "French", "Hungarian", "Belgian", "Dutch", "Italian", "Singapore", "Japanese", "United States", "Mexico City", "são Paulo", "Abu Dhabi", NULL};
 
-//"class" grand prix
+//"class" grand prix (GP)
 struct GrandPrix {
 	char* location;
 
@@ -27,18 +27,40 @@ struct GrandPrix {
 //une voiture est définie par son numéro est son total de points
 struct Car {
 	int number;
-	int points;
+	int points; //(total)
+
+	int* points_GP; //list of points gained during each GP
+	int* best_laps; //list of best lap in order of GP
 };
 
+//generate a random time corresponding to the time spent
+//traversing 1/3 of a lap
 int get_time(int min, int max) {
 	return (rand() % (21)) + 25;
 };
 
+
+int length(int*** list) {
+
+}
+
+
+
 //=====MAIN=====
+
+//display the given 2D matrix containing strings
+//	(first line is collum names)
+//	(everything must be NULL terminated)
+void display(char*** board) {
+	int* lenghts[]
+	for (i=0; board[i] != NULL; i++) {
+	}
+};
 
 void main() {
 	//initialise random number generator
 	srand(time(0));
+
 
 	//go through cars numbers to create them
 	Cars* cars = malloc(sizeof(Car));
@@ -57,6 +79,7 @@ void main() {
 		cars[i].points = 0;
 	}
 
+
 	//go through locations to create and run each grand prix
 	GrandPrix* grandprix = malloc(sizeof(GrandPrix));
 	grandPrix[0] = NULL;
@@ -66,12 +89,11 @@ void main() {
 		//add cup to list
 		struct GrandPrix grandprix[i];
 		cup_number++;
-		grandprix = realloc(sizeof(GrandPrix) * cup_number+1);
+		grandprix = realloc(sizeof(GrandPrix) * i+1);
 		grandprix[i+1] = NULL;
 
 		//initialize vars
 		grandprix[i].location = locations[i];
-
 	}
 
 };
